f8e2a0a2b9e67c50c950f2bc4d1efb4401a4bad5
Kasun Gamage <kasun.gamage@blackswan-technologies.com>
2021-09-12 21:25:45 +0530

    service changes

--- src/app/modules/auth/login/login.page.ts
+++ src/app/modules/auth/login/login.page.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { UserService } from '../../../services/api/user.service';
-import { ToastService } from '../../../services/utilities/toast.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from "@angular/forms";
+import { UserService } from "../../../services/api/user.service";
+import { ToastService } from "../../../services/utilities/toast.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"]
 })
 export class LoginPage implements OnInit {
   userLoginForm: FormGroup;
-  verificationCode: FormControl = new FormControl('', [Validators.minLength(4), Validators.maxLength(4)]);
+  verificationCode: FormControl = new FormControl("", [
+    Validators.minLength(4),
+    Validators.maxLength(4)
+  ]);
   isVerificationCodeSend = false;
+  requestId: any;
   constructor(
     public formBuilder: FormBuilder,
     private toastService: ToastService,
@@ -20,31 +29,32 @@ export class LoginPage implements OnInit {
 
   ngOnInit() {
     this.userLoginForm = this.formBuilder.group({
-      mobile: ['0717935364', [Validators.required, Validators.minLength(10)]]
+      mobile: ["0717935364", [Validators.required, Validators.minLength(10)]]
     });
   }
 
   sendVerificationCode() {
     if (!this.userLoginForm.valid) {
       this.toastService.presentToastWithOptions(
-        'Please provide all the required values!'
+        "Please provide all the required values!"
       );
       return false;
     } else {
       this.userService
         .sendVerificationCode(this.userLoginForm.value.mobile)
-        .subscribe(
-          (res: any) => {
-            this.toastService.presentToastWithOptions(
-              'Verification code send Successfully!'
-            );
-            this.isVerificationCodeSend = true;
-          }
-        );
+        .subscribe((res: any) => {
+          this.toastService.presentToastWithOptions(
+            "Verification code send Successfully!"
+          );
+          this.requestId = res.requestId;
+          this.isVerificationCodeSend = true;
+        });
     }
   }
 
-  verifiyCode(): void {
-    //
+  verifyCode(): void {
+    this.userService
+      .verifyCode(this.requestId, this.verificationCode.value)
+      .subscribe((res: any) => {});
   }
 }
--- src/app/services/api/user.service.ts
+++ src/app/services/api/user.service.ts
@@ -1,42 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { HttpService } from './http.service';
-import { environment } from '../../../environments/environment';
-import { UserAdd } from '../../model/user';
+import { HttpService } from "./http.service";
+import { environment } from "../../../environments/environment";
+import { UserAdd } from "../../model/user";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root"
 })
 export class UserService {
   constructor(private httpService: HttpService) {}
 
   add(user: UserAdd) {
-    const apiURL = environment.url + 'users/add';
+    const apiURL = environment.url + "users/add";
     return this.httpService.post(apiURL, user);
   }
 
   update(id: string, user: UserAdd) {
-    const apiURL = environment.url + 'users/update?userId=' + id;;
+    const apiURL = environment.url + "users/update?userId=" + id;
     return this.httpService.put(apiURL, user);
   }
 
   delete(id: string) {
-    const apiURL = environment.url + 'users/delete?userId=' + id;;
+    const apiURL = environment.url + "users/delete?userId=" + id;
     return this.httpService.delete(apiURL);
   }
 
   findById(id: string) {
-    const apiURL = environment.url + 'users/findById?userId=' + id;
+    const apiURL = environment.url + "users/findById?userId=" + id;
     return this.httpService.get(apiURL);
   }
 
   getAll() {
-    const apiURL = environment.url + 'users/getAll';
+    const apiURL = environment.url + "users/getAll";
     return this.httpService.get(apiURL);
   }
 
   sendVerificationCode(mobile: number) {
-    const apiURL = environment.url + 'users/verification?mobile=' + mobile;
+    const apiURL = environment.url + "users/verification?mobile=" + mobile;
+    return this.httpService.get(apiURL);
+  }
+
+  verifyCode(requestId: string, code: string) {
+    const apiURL =
+      environment.url + `users/verifyCode?requestId=${requestId}&code=${code}`;
     return this.httpService.get(apiURL);
   }
 }
